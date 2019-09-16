d5667e416aea9862b3c347fa0de964c749ecc80f
crossingmay <mcxin001@163.com>
2019-09-16 09:53:58 +0800

    防止另存为，禁止查看源码

--- src/common/util-page.js
+++ src/common/util-page.js
@@ -52,12 +52,52 @@ function banCtxmenu () {
 
 // 防止另存为
 function banSave () {
-  // todo
+  // 禁止右键另存为，同上方法
+  // 禁止复制
+  document.onselectstart = function (e) {
+    e.preventDefault()
+  }
+  // 或在body中加入相关html代码
+  // 限制右键和选择
+  // <body oncontextmenu="return false" onselectstart="return false">
+  // <body oncontextmenu="event.returnValue=false" onselectstart="event.returnValue=false">
+  // 限制复制
+  // <body oncopy="alert('对不起，禁止复制！');return false;">
+  // 使菜单”文件”－”另存为”失效
+  // <noscript>
+  //   <iframe src="*.htm"></iframe>
+  // </noscript>
 }
 
-// 禁止查看源码
+// 禁止查看源码-只能防菜鸟
 function banViewSource (e) {
-  // todo
+  // 三种查看方式，然后作出对应限制措施
+  // 1、直接按F12
+  // 2、Ctrl+Shift+I（或Cmd+Option+I）查看
+  // 3、鼠标点击右键查看
+
+  // 屏蔽键盘事件
+  document.onkeydown = function () {
+    var e = window.event || arguments[0]
+    // F12
+    if (e.keyCode == 123) {
+      return false
+      // Ctrl+Shift+I
+    } else if ((e.ctrlKey) && (e.shiftKey) && (e.keyCode === 73)) {
+      return false
+      // Shift+F10
+    } else if ((e.shiftKey) && (e.keyCode === 121)) {
+      return false
+      // Ctrl+U
+    } else if ((e.ctrlKey) && (e.keyCode === 85)) {
+      return false
+    }
+    // ...
+  }
+  // 屏蔽鼠标右键
+  document.oncontextmenu = function () {
+    return false
+  }
 }
 
 // 添加到收藏夹
